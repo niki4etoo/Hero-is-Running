e9848258916e86a3f2a6d3a441c07bf38890167b
Nikolay Nanev <niki4etooo@gmail.com>
2020-05-01 18:40:03 +0300

    Update main.c

--- main.c
+++ main.c
@@ -4,5 +4,27 @@ int main(int argc, char** argv)
 {
 	printf("The number of arguments are %d\n", argc);
 	printf("The first argument that you enter is %s\n", argv[1]);
+	
+	char is_running = 0;
+	
+	if(argv[1] == 1)
+	{
+		is_running = 1;	//Set is_running to true
+	}
+	else
+	{
+		is_running = -1;
+	}
+	
+	if(is_running)
+	{
+		printf("Hero is set to run!\n");
+	}
+	
+	if(is_running == -1)
+	{
+		printf("Hero is waiting for your commands!\n);
+	}
+				
 	return 0;
 }
