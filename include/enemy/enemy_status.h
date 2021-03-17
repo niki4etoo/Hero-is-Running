c22bd784cbbe0f9328fc3c0efb38b8eb6c443ee4
Nikolai Nanev <niki4etooo@gmail.com>
2021-03-17 06:26:03 +0200

    added setters/getters for enemy status

--- include/enemy/enemy_status.h
+++ include/enemy/enemy_status.h
@@ -14,8 +14,18 @@ class EnemyStatus {
 public:
 	EnemyStatus(size_t id, std::string name, size_t health);
 	~EnemyStatus();
-	bool isAlive();
+	bool isAlive();	
 	void pointsTaken();
+	
+	void setID(size_t id);
+	void setName(std::string name);
+	void setHealth(size_t health);
+	void setPoints(size_t points);
+	
+	size_t getID();
+	std::string getName();
+	size_t getHealth();
+	size_t getPoints();	
 };
 
 #endif
--- src/enemy/enemy_status.cpp
+++ src/enemy/enemy_status.cpp
@@ -13,3 +13,39 @@ bool EnemyStatus::isAlive(){
 void EnemyStatus::pointsTaken(){
 	std::cout << "Points taken: " << this->points << std::endl;
 }
+
+//Setters
+
+void EnemyStatus::setID(size_t id){
+	this->id = id;
+}
+
+void EnemyStatus::setName(std::string name){
+	this->name = name;
+}
+
+void EnemyStatus::setHealth(size_t health){
+	this->health = health;
+}
+
+void EnemyStatus::setPoints(size_t points){
+	this->points = points;
+}
+
+//Getters
+
+size_t EnemyStatus::getID(){
+	return this->id;
+}
+
+std::string EnemyStatus::getName(){
+	return this->name;
+}
+
+size_t EnemyStatus::getHealth(){
+	return this->health;
+}
+
+size_t EnemyStatus::getPoints(){
+	return this->points;
+}
