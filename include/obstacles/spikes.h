b1a1569ba9f351dfbf77eef2dc84dbea2e1299c0
Nikolai Nanev <niki4etooo@gmail.com>
2021-05-24 21:33:47 +0300

    added posX&Y and count for spikes

--- include/obstacles/spikes.h
+++ include/obstacles/spikes.h
@@ -7,6 +7,8 @@
 class Spikes {
 	std::string type;
 	size_t width, height;
+	size_t count;
+	size_t posX, posY;
 	//To do ( Texture load of spike )
 public:
 	Spikes(){};
@@ -16,11 +18,17 @@ public:
 	void setType(std::string type);
 	void setWidth(size_t width);
 	void setHeight(size_t height);
+	void setCount(size_t count);
+	void setPosX(size_t posX);
+	void setPosY(size_t posY);
 	
 	//Getters
 	std::string getType();
 	size_t getWidth();
 	size_t getHeight();
+	size_t getCount();
+	size_t getPosX();
+	size_t getPosY();
 };
 
 #endif
--- src/obstacles/spikes.cpp
+++ src/obstacles/spikes.cpp
@@ -13,6 +13,18 @@ void Spikes::setHeight(size_t height){
 	this->height = height;
 }
 
+void Spikes::setCount(size_t count){
+	this->count = count;
+}
+
+void Spikes::setPosX(size_t posX){
+	this->posX = posX;
+}
+
+void Spikes::setPosY(size_t posY){
+	this->posY = posY;
+}
+
 //Getters
 std::string Spikes::getType(){
 	this->type = type;
@@ -24,4 +36,15 @@ size_t Spikes::getWidth(){
 
 size_t Spikes::getHeight(){
 	this->height = height;
+}
+
+size_t Spikes::getCount(){
+	this->count = count;
+}
+
+size_t Spikes::getPosX(){
+	this->posX = posX;
+}
+size_t Spikes::getPosY(){
+	this->posY = posY;
 }
